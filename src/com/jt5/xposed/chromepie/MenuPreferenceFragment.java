JayT5/ChromePie

commit c32b11d4f76de75ae8a70a9623740afefc2be206

Move menu item modification to a separate fragment

src/com/jt5/xposed/chromepie/MenuPreferenceFragment.java
@@ -1,0 +1,125 @@
+package com.jt5.xposed.chromepie;
+
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.view.MenuItem;
+import android.widget.BaseAdapter;
+
+import com.jt5.xposed.chromepie.preference.PieMainPreference;
+
+public class MenuPreferenceFragment extends PreferenceFragment {
+
+    private SharedPreferences mSharedPrefs;
+    private PreferenceCategory mPieMenuCat;
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
+        setHasOptionsMenu(true);
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        addPreferencesFromResource(R.xml.menu_preferences);
+
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        if (!mSharedPrefs.contains("screen_slice_1")) {
+            loadDefaultValues(false);
+        }
+
+        mPieMenuCat = (PreferenceCategory) findPreference("pie_slices_cat");
+
+        final Preference newSlice = findPreference("new_slice");
+        newSlice.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                if (mPieMenuCat.getPreferenceCount() < 6) {
+                    PieMainPreference mainPref = new PieMainPreference(getActivity(), null, (mPieMenuCat.getPreferenceCount() + 1));
+                    mPieMenuCat.addPreference(mainPref);
+                }
+                return true;
+            }
+        });
+
+        loadPreferences();
+
+    }
+
+    @SuppressWarnings("deprecation")
+    private void loadDefaultValues(boolean readAgain) {
+        PreferenceManager.setDefaultValues(getActivity(), getPreferenceManager().getSharedPreferencesName(),
+                Context.MODE_WORLD_READABLE, R.xml.aosp_preferences, readAgain);
+
+        if (readAgain) {
+            Editor editor = mSharedPrefs.edit();
+            for (int i = 1; i < 6; i++) {
+                editor.putBoolean("screen_slice_" + i, true);
+            }
+            editor.putBoolean("screen_slice_6", false).apply();
+        }
+    }
+
+    private void loadPreferences() {
+        Map<String, ?> keys = mSharedPrefs.getAll();
+        for (Map.Entry<String,?> entry : keys.entrySet()) {
+            String key = entry.getKey();
+            if (key.startsWith("screen_") && (Boolean) entry.getValue()) {
+                int slice = Character.getNumericValue(key.charAt(key.length() - 1));
+                PieMainPreference mainPref = new PieMainPreference(getActivity(), null, slice);
+                mPieMenuCat.addPreference(mainPref);
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference instanceof PieMainPreference) {
+            Bundle extras = new Bundle();
+            extras.putInt("slice", ((PieMainPreference) preference).getSlice());
+            extras.putInt("count", mPieMenuCat.getPreferenceCount());
+            ((PreferenceActivity) getActivity()).startWithFragment(preference.getFragment(), extras, this, 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            int slice = data.getIntExtra("slice", 1);
+            String summary = data.getStringExtra("entry");
+            PieMainPreference pref = (PieMainPreference) findPreference("screen_slice_" + slice);
+            pref.setSummary(summary);
+        }
+        ((BaseAdapter) getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_load_defaults:
+                mPieMenuCat.removeAll();
+                loadDefaultValues(true);
+                loadPreferences();
+                return true;
+            case android.R.id.home:
+                ((PreferenceActivity) getActivity()).finishPreferencePanel(this, Activity.RESULT_OK, null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}

src/com/jt5/xposed/chromepie/PiePreferenceFragment.java
@@ -1,13 +1,8 @@
 package com.jt5.xposed.chromepie;
 
-import java.util.Map;
-
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -15,51 +10,26 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import android.view.MenuItem;
-import android.widget.BaseAdapter;
-
-import com.jt5.xposed.chromepie.preference.PieMainPreference;
 
 public class PiePreferenceFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 
     private SharedPreferences mSharedPrefs;
-    private PreferenceCategory pieSlicesCat;
 
     @SuppressWarnings("deprecation")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
         getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
         addPreferencesFromResource(R.xml.main_preferences);
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        if (!mSharedPrefs.contains("screen_slice_1")) {
-            loadDefaultValues(false);
-        }
 
-        pieSlicesCat = (PreferenceCategory) findPreference("pie_slices_cat");
         final Preference killChrome = findPreference("kill_chrome");
         killChrome.setOnPreferenceClickListener(new OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 ((PieSettings) getActivity()).killChrome(true);
-                return true;
-            }
-        });
-
-        final Preference newSlice = findPreference("new_slice");
-        newSlice.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                if (pieSlicesCat.getPreferenceCount() < 6) {
-                    PieMainPreference mainPref = new PieMainPreference(getActivity(), null, (pieSlicesCat.getPreferenceCount() + 1));
-                    pieSlicesCat.addPreference(mainPref);
-                }
                 return true;
             }
         });
@@ -76,73 +46,9 @@ public class PiePreferenceFragment extends PreferenceFragment implements OnShare
             }
         });
 
-        loadPreferences();
-    }
-
-    @SuppressWarnings("deprecation")
-    private void loadDefaultValues(boolean readAgain) {
-        PreferenceManager.setDefaultValues(getActivity(), getPreferenceManager().getSharedPreferencesName(),
-                Context.MODE_WORLD_READABLE, R.xml.aosp_preferences, readAgain);
-
-        if (readAgain) {
-            Editor editor = mSharedPrefs.edit();
-            for (int i = 1; i < 6; i++) {
-                editor.putBoolean("screen_slice_" + i, true);
-            }
-            editor.putBoolean("screen_slice_6", false).apply();
-        }
-    }
-
-    private void loadPreferences() {
-        Map<String, ?> keys = mSharedPrefs.getAll();
-
-        for (Map.Entry<String,?> entry : keys.entrySet()) {
-            String key = entry.getKey();
-            if (key.startsWith("screen_") && (Boolean) entry.getValue()) {
-                int slice = Character.getNumericValue(key.charAt(key.length() - 1));
-                PieMainPreference mainPref = new PieMainPreference(getActivity(), null, slice);
-                pieSlicesCat.addPreference(mainPref);
-            }
-        }
-
         final ListPreference triggerSide = (ListPreference) findPreference("trigger_side");
         triggerSide.setSummary(triggerSide.getEntry());
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_load_defaults:
-                pieSlicesCat.removeAll();
-                loadDefaultValues(true);
-                loadPreferences();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference instanceof PieMainPreference) {
-            Bundle extras = new Bundle();
-            extras.putInt("slice", ((PieMainPreference) preference).getSlice());
-            extras.putInt("count", pieSlicesCat.getPreferenceCount());
-            ((PreferenceActivity) getActivity()).startWithFragment(preference.getFragment(), extras, this, 0);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == Activity.RESULT_OK) {
-            int slice = data.getIntExtra("slice", 1);
-            String summary = data.getStringExtra("entry");
-            PieMainPreference pref = (PieMainPreference) findPreference("screen_slice_" + slice);
-            pref.setSummary(summary);
-        }
-        ((BaseAdapter) getPreferenceScreen().getRootAdapter()).notifyDataSetChanged();
     }
 
     @Override

res/values/strings.xml
@@ -28,9 +28,11 @@
     <string name="chrome_killed">Chrome killed</string>
     <string name="cannot_launch_chrome">Cannot launch Chrome</string>
     
+    <string name="edit_pie_menu_title">Pie menu</string>
+    <string name="edit_pie_menu_summary">Edit Pie menu items</string>
+    
     <string name="new_slice">Add new slice</string>
     <string name="pie_slices_cat">Pie Slices</string>
-    <string name="other_cat_title">Other</string>
     <string name="hide_launcher_icon_title">Hide launcher icon</string>
     
     <string name="trigger_side">Trigger side</string>

res/xml/main_preferences.xml
@@ -6,6 +6,11 @@
         android:key="kill_chrome"
         android:title="@string/kill_chrome"
         android:summary="@string/kill_chrome_summary" />
+    <Preference
+        android:key="edit_pie_menu"
+        android:title="@string/edit_pie_menu_title"
+        android:summary="@string/edit_pie_menu_summary"
+        android:fragment="com.jt5.xposed.chromepie.MenuPreferenceFragment" />
     <ListPreference
         android:key="trigger_side"
         android:title="@string/trigger_side"
@@ -24,16 +29,6 @@
         android:defaultValue="@integer/qc_slop"
         minimum="5"
         maximum="30" />
-    <PreferenceCategory
-        android:key="pie_slices_cat"
-        android:title="@string/pie_slices_cat" />
-    <Preference
-        android:key="new_slice"
-        android:title="@string/new_slice"
-        android:icon="@drawable/ic_action_new" />
-    <PreferenceCategory
-        android:key="other_cat"
-        android:title="@string/other_cat_title" />
     <CheckBoxPreference
         android:key="hide_launcher_icon"
         android:title="@string/hide_launcher_icon_title"

res/xml/menu_preferences.xml
@@ -1,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<PreferenceScreen xmlns:android="http://schemas.android.com/apk/res/android" 
+    android:name="pref_screen_name" android:key="pref_screen_key">
+
+    <PreferenceCategory
+        android:key="pie_slices_cat"
+        android:title="@string/pie_slices_cat" />
+    <Preference
+        android:key="new_slice"
+        android:title="@string/new_slice"
+        android:icon="@drawable/ic_action_new" />
+
+</PreferenceScreen>

src/com/jt5/xposed/chromepie/PieSettings.java
@@ -51,7 +51,8 @@ public class PieSettings extends PreferenceActivity {
 
     @Override
     protected boolean isValidFragment(String fragmentName) {
-      if (SubPreferenceFragment.class.getName().equals(fragmentName)) {
+      if (SubPreferenceFragment.class.getName().equals(fragmentName) ||
+              MenuPreferenceFragment.class.getName().equals(fragmentName) ) {
           return true;
       }
       return false;

commit a3028c7a0ebb523ff71d4c663526aa212ed0f6b8

Fix load default values on new installs
Issue caused by c32b11d4f76de75ae8a70a9623740afefc2be206

src/com/jt5/xposed/chromepie/MenuPreferenceFragment.java
@@ -33,12 +33,7 @@ public class MenuPreferenceFragment extends PreferenceFragment {
         setHasOptionsMenu(true);
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
         addPreferencesFromResource(R.xml.menu_preferences);
-
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        if (!mSharedPrefs.contains("screen_slice_1")) {
-            loadDefaultValues(false);
-        }
-
         mPieMenuCat = (PreferenceCategory) findPreference("pie_slices_cat");
 
         final Preference newSlice = findPreference("new_slice");

src/com/jt5/xposed/chromepie/PiePreferenceFragment.java
@@ -27,6 +27,13 @@ public class PiePreferenceFragment extends PreferenceFragment implements OnShare
         addPreferencesFromResource(R.xml.main_preferences);
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
+        // If SharedPreferences does not contain this preference,
+        // we can presume this is a new install
+        if (!mSharedPrefs.contains("screen_slice_1")) {
+            PreferenceManager.setDefaultValues(getActivity(), getPreferenceManager().getSharedPreferencesName(),
+                    Context.MODE_WORLD_READABLE, R.xml.aosp_preferences, false);
+        }
+
         final Preference killChrome = findPreference("kill_chrome");
         killChrome.setOnPreferenceClickListener(new OnPreferenceClickListener() {
             @Override