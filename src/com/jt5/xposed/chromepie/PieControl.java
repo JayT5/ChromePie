fb58a48574df2387f6336f589a930c400aa02cf8
JayT5 <JayT5@users.noreply.github.com>
2016-01-05 22:36:10 +0000

    Add recent apps to actions that work with no tabs open

--- src/com/jt5/xposed/chromepie/PieControl.java
+++ src/com/jt5/xposed/chromepie/PieControl.java
@@ -71,7 +71,8 @@ public class PieControl implements PieMenu.PieController {
         mXPreferences = prefs;
         mXPreferences.reload();
         mItemSize = mXResources.getDimensionPixelSize(R.dimen.qc_item_size);
-        mNoTabActions = Arrays.asList("new_tab", "new_incognito_tab", "fullscreen", "settings", "exit", "go_to_home", "show_tabs");
+        mNoTabActions = Arrays.asList("new_tab", "new_incognito_tab", "fullscreen",
+                "settings", "exit", "go_to_home", "show_tabs", "recent_apps");
         mTriggerPositions = initTriggerPositions();
         if (!mController.isDocumentMode() && mXPreferences.getBoolean("toolbar_apply_theme_color", true)) {
             initializeUIHook();
